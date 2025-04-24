0fbc801c786ed86416bbdfa47534f5fbf7ab2c8f
dobriev <dobromirv@proton.me>
2025-04-24 19:31:56 +0300

    @Entity with @Data isn't production-safe, removed

--- src/main/java/com/dobval/open_info_hub/entities/AggregatedInfo.java
+++ src/main/java/com/dobval/open_info_hub/entities/AggregatedInfo.java
@@ -1,25 +1,36 @@
 package com.dobval.open_info_hub.entities;
 
-import java.time.LocalDateTime;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Entity
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class AggregatedInfo {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String username;
     private int githubRepos;
     private int githubFollowers;
+
     private String city;
     private String weather;
+
     private double eurToUsdRate;
+
     private LocalDateTime fetchedAt;
 }
-
